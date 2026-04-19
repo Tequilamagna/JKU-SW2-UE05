14a9fb73c20ab61658d5b98275f44a605304615f
Tequilamagna <aquilamagna@gmx.at>
2026-04-19 19:59:19 +0200

    ColumnIterable implemented

--- src/tabular/Table.java
+++ src/tabular/Table.java
@@ -22,17 +22,53 @@ public class Table implements Tabular{
 
     @Override
     public Iterable<Integer> iterableRow(int row) {
+        if(row < 0 || row >= table.length) {
+            throw new IndexOutOfBoundsException("this index does not exist");
+        }
         return new RowIterable(table[row]);
     }
 
     @Override
     public Iterable<Integer> iterableCol(int col) {
-        return null;
+        if(col < 0 || col >= table[0].length) {
+            throw new IndexOutOfBoundsException("this index does not exist");
+        }
+        return new ColumnIterable(col);
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return null;
+    }
+
+    public class ColumnIterable implements Iterable<Integer> {
+        int j;
+
+        private ColumnIterable(int j) {
+            this.j = j;
+        }
+
+        @Override
+        public Iterator<Integer> iterator() {
+            return new Iterator<>() {
+                int i = 0;
+
+                @Override
+                public boolean hasNext() {
+                    return i < table.length;
+                }
+
+                @Override
+                public Integer next() {
+                    if(!this.hasNext()) {
+                        throw new NoSuchElementException("this is not the next you're looking for.");
+                    }
+                    int result = table[i][j];
+                    i++;
+                    return result;
+                }
+            };
+        }
     }
 
     public static class RowIterable implements Iterable<Integer> {
@@ -43,7 +79,7 @@ public class Table implements Tabular{
         }
         @Override
         public Iterator<Integer> iterator() {
-            return new Iterator<Integer>() {
+            return new Iterator<>() {
                 int i = 0;
 
                 @Override
